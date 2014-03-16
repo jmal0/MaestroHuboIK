6e698ce26a01990e1bfc2406eadf4470dc1da687
jmal0 <jmal0320@gmail.com>
2014-03-16 16:34:35 -0400

    Test script to make a hip thrusting trajectory. This is how I choose to spend my weekend

--- test.m
+++ test.m
@@ -1,12 +1,28 @@
 clear
 
-rightLeg = Leg(Leg.RIGHT);
-tm = TrajMaker(rightLeg.joints);
-rightLeg.joints
-[hy, hr, hp, kp, ap, ar] = rightLeg.setFootLowerBodyXYZ(-.05, -.05, -.4, 1.57);
-data = [hy, hr, hp, kp, ap, ar];
-success = tm.addLine(data);
-[hy, hr, hp, kp, ap, ar] = rightLeg.setFootLowerBodyXYZ(0, -.05, -.4, 0);
-data = [hy, hr, hp, kp, ap, ar];
-success = tm.addLine(data);
-z = tm.angles;
+leftLeg = Leg(Leg.LEFT, Leg.GLOBAL);
+rightLeg = Leg(Leg.RIGHT, Leg.GLOBAL);
+leftHipThrust = IKTraj(leftLeg.joints);
+rightHipThrust = IKTraj(rightLeg.joints);
+
+t1 = 0:.001:.1;
+t2 = .1:-.001:-.1;
+t3 = -.1:.001:0;
+x = [t1,t2,t3];
+y = zeros(1, length(x));
+z = zeros(1, length(x)) - .45;
+
+for i = 1:length(x)
+    dataL = leftLeg.setXYZ(x(i), y(i), z(i), 0);
+    dataR = rightLeg.setXYZ(x(i), y(i), z(i), 0);
+    if(or(isnan(dataL), isnan(dataR)))
+        print('fail')
+        return
+    end
+    leftHipThrust.addLine(dataL);
+    rightHipThrust.addLine(dataR);
+end
+
+tm = TrajMaker(cellstr('test'), []);
+tm.joinFromStart(leftHipThrust, rightHipThrust);
+tm.createTrajectory('hipThrustTest.traj')
