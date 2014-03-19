0cfdec28b741d498daeab1c48db355a1d4e20d33
jmal0 <jmal0320@gmail.com>
2014-03-18 22:27:30 -0400

    fk. I'd do ik but im tired

--- Arm.m
+++ Arm.m
@@ -1,20 +1,29 @@
 classdef Arm < Limb
     properties (Constant)
-    	RIGHT = 1;
-    	LEFT = 0;
+    	RIGHT = 0;
+    	LEFT = 1;
     	% Limb lengths
     	UPPER_ARM_X = .02;
     	UPPER_ARM_Z = .182;
     	LOWER_ARM = .1655;
+        % Joint offsets
+        SR_OFFSET = .2618;
+        ELBOW_OFFSET = .0411;
 	end
 
 	properties
 		% Shoulder pitch to wrist pitch transform
 		T_04;
+        sr_off;
 	end
 
 	methods
 		function arm = Arm(which)
+            if(which)
+                arm.sr_off = -Arm.SR_OFFSET;
+            else
+                arm.sr_off = Arm.SR_OFFSET;
+            end
 			% Symbolicly solve transformation matrices
             % SP: Shoulder pitch
             % SR: Shoulder roll
@@ -35,34 +44,34 @@ classdef Arm < Limb
             		0,			-1,	0,			0;...
             		0,			0,	0,			1];
             % Shoulder yaw to elbow pitch transform
-            A_3 = [	sin(SY),	0,	cos(SY),	Leg.UPPER_ARM_X*sin(SY);...
-            		-cos(SY),	0,	sin(SY),	-Leg.UPPER_ARM_X*cos(SY);...
-            		0,			-1,	0,			-Leg.UPPER_ARM_Z;...
+            A_3 = [	sin(SY),	0,	cos(SY),	Arm.UPPER_ARM_X*sin(SY);...
+            		-cos(SY),	0,	sin(SY),	-Arm.UPPER_ARM_X*cos(SY);...
+            		0,			-1,	0,			-Arm.UPPER_ARM_Z;...
             		0,			0,	0,			1];
             % Elbow pitch to wrist pitch transform
-            A_4 = [	-sin(EP),	-cos(EP),	0,	-Leg.LOWER_ARM*sin(EP);...
-            		cos(EP),	-sin(EP),	0,	Leg.LOWER_ARM*cos(EP);...
+            A_4 = [	-sin(EP),	-cos(EP),	0,	-Arm.LOWER_ARM*sin(EP);...
+            		cos(EP),	-sin(EP),	0,	Arm.LOWER_ARM*cos(EP);...
             		0,			0,			1,	0;...
             		0,			0,			0,	1];
-            arm.T_14 = symfun(A_1*A_2*A_3*A_4, [SP SR SY EP])
+            arm.T_04 = symfun(A_1*A_2*A_3*A_4, [SP SR SY EP])
 
             arm.joints = cellstr(['RSP';'RSR';'RSY';'REP']);
 
         end
 
-        function [x,y,z] = getXYZ(arm, data)
-            pitch = xyzData(1);
-            roll = xyzData(2);
-            yaw = xyzData(3);
-            elbow = xyzData(4);
+        function [x,y,z] = getXYZ(arm, jointData)
+            pitch = jointData(1);
+            roll = jointData(2) - arm.sr_off;
+            yaw = jointData(3);
+            elbow = jointData(4) - Arm.ELBOW_OFFSET;
             t = arm.T_04(pitch, roll, yaw, elbow);
-            end
-            x = eval(t(1,4));
-            y = eval(t(2,4));
-            z = eval(t(3,4));
+            x = -eval(t(2,4));
+            y = eval(t(3,4));
+            z = -eval(t(1,4));
         end
 
         function jointData = setXYZ(arm, xyzData)
             jointData = [0,0,0,0];
-	end
+        end
+    end
 end
